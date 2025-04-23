98940d88cb322087806931f1fb61c4d8d8e149a4
Nele M <nelemeyer43@gmail.com>
2025-04-23 11:24:40 +0200

    writeBooks, showBooks function, booklist main page, CSS for read books

---
+++ bookfunctions.php
@@ -0,0 +1,36 @@
+<?php 
+
+function writeBook($title, $author, $date, $notes, $rating, $image) {
+    $datei = fopen("books.txt", "a");
+    $zeile = $title . ";" . $author . ";" . $date . ";" . $notes . ";" . $rating . ";" . $image . "\n";
+    fwrite($datei, $zeile);
+    fclose($datei);
+}
+
+
+
+function showBooks($filename) {
+    if (file_exists($filename)) {
+        $file = fopen($filename, "r");
+
+        echo '<div class="book-grid">';
+        while (!feof($file)) {
+            $line = fgets($file);
+
+            if (!empty(trim($line))) {
+                list($title, $author, $date, $notes, $rating, $image) = explode(";", $line);
+                echo '<div class="book-card">';
+                echo "<img src=\"$image\" alt=\"Cover of $title\">";
+                echo "<h3>$title</h3>";
+                echo "<p><strong>$author</strong></p>";
+                echo "<p>$date</p>";
+                echo "<p>$notes</p>";
+                echo "<p class='stars'>" . str_repeat("★", intval($rating)) . "</p>";
+                echo '</div>';
+            }
+        }
+        echo '</div>';
+
+        fclose($file); 
+    }
+}
---
+++ booklist.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Neles Bookshelf</title>
+    <link rel="stylesheet" href="style.css"> 
+</head>
+<body>
+
+<h1>Neles Bookshelf</h1>
+
+<h2>Add a book</h2>
+
+<form action="" method="POST">
+    Titel: <input type="text" name="title" value=""><br>
+    Autor: <input type="text" name="author" value=""><br>
+    Datum: <input type="date" name="date" value=""><br>
+    Notizen: <input type="text" name="notes" value=""><br>
+    <label for="rating">Bewertung:</label>
+        <select name="rating" id="rating">
+            <option value=" "> </option>
+            <option value="1">★</option>
+            <option value="2">★★</option>
+            <option value="3">★★★</option>
+            <option value="4">★★★★</option>
+            <option value="5">★★★★★</option>
+        </select><br>
+    Bild-URL: <input type="text" name="image" value=""><br>
+    <input type="submit" value="Save book">
+</form>
+
+<h2>Read Books</h2>
+
+<?php
+include("bookfunctions.php");
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    if (!empty($_POST["title"]) && !empty($_POST["author"])) {
+        writeBook($_POST["title"], $_POST["author"], $_POST["date"], $_POST["notes"], $_POST["rating"], $_POST["image"]);
+        header("Location: " . $_SERVER['PHP_SELF']); // reload same page to clear form - PHP_SELF means samepage
+        exit(); // end script, so data is not saved again
+        }
+        
+    }
+
+showBooks("books.txt");
+?>
+
+
+</body>
+</html>
